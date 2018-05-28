df7ae9b18157a9a7008f55fcd4517a5c0c60af67
ARCassiano <anderson.cassiano@hotmail.com>
2018-05-28 18:41:14 -0300

    Uso de sessão para validar o login

--- PHPIniciante/login/controller.php
+++ PHPIniciante/login/controller.php
@@ -1,12 +1,15 @@
 <?php
 
-	//$titulo	= "Aula 13 - Curso de PHP Básico";
+	$titulo	= "Aula 13 - Curso de PHP Básico";
 
 	//Se o usuário não estivwe logado, devemos retornar o formulario de login
 	//Se o usuário estiver logado retornar tela de boas vindas com o seu nome 	
 
 	//Iniciar o uso de sessões
 	session_start();
+
+	if(isset($_POST["usuario"]) && isset($_POST["senha"]) && $_POST["senha"] == "admin" && $_POST["usuario"] == "admin")
+		$_SESSION["usuario"]	= $_POST["usuario"];
 
 	if(isset($_SESSION["usuario"])){
 		require_once("tmpl_administrativo.php");
--- PHPIniciante/login/tmpl_formularioLogin.php
+++ PHPIniciante/login/tmpl_formularioLogin.php
@@ -4,12 +4,15 @@
 	<title><?= $titulo ?></title>
 </head>
 <body>
+	<p>Efetue o login para ter acesso ao sistema</p>
 	<form action="" method="post">
 		<p>Usuário: </p>
 		<input type="text" name="usuario">
 
 		<p>Senha: </p>
 		<input type="password" name="senha">
+
+		<input type="submit" name="ok">
 	</form>
 </body>
 </html>
