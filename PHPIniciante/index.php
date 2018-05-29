9b47969b49a3f631795e423f9471b341d03fc9fe
ARCassiano <anderson.cassiano@hotmail.com>
2018-05-28 21:57:52 -0300

    Utilizando Header para redirecionar página e bloquear IPs bloqueados

--- PHPIniciante/index.php
+++ PHPIniciante/index.php
@@ -1,5 +1,15 @@
 <?php 
 
+	//Verirficar se o IP está em nossa blocklist
+	$ipsbloqueados	= array("170.82.135.211");
+
+	foreach ($ip as $ipsbloqueados) {
+		if($ip == $_SERVER["REMOTE_ADDR"]){
+			header("Location: ./negado.php");
+			exit();
+		}
+	}
+
 	$r = $_GET["r"];
 
 	require_once("funcoes.php");
