d1867f748709901e65adff2ceace7fa5400a4e72
ARCassiano <anderson.cassiano@hotmail.com>
2018-05-31 12:07:48 -0300

    Forçando a falha na conexão

--- PHPIniciante/mysql/controller.php
+++ PHPIniciante/mysql/controller.php
@@ -2,11 +2,11 @@
 
 	$titulo	= "Aula 18 - Curso de PHP para iniciantes";
 
-	$conexao	= mysqli_connect($dbHost . "as", $dbUsername, $dbPassword, $dbName);
+	$conexao	= @mysqli_connect($dbHost . "as", $dbUsername, $dbPassword, $dbName);
 
 	if(mysqli_connect_errno($conexao))
 		$resultado	= "A conexão falhou, erro reportado: " . mysqli_connect_error();
 	else
 		$resultado	= "Conexão realizada com sucesso!";
 
-	mysqli_close($conexao);
+	@mysqli_close($conexao);
