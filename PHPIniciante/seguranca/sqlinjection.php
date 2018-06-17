a80b78c14748ff572e676fb930c762b5ed74f97f
ARCassiano <anderson.cassiano@hotmail.com>
2018-06-17 17:54:27 -0300

    Exemplo de SQL Injection

---
+++ PHPIniciante/seguranca/sqlinjection.php
@@ -0,0 +1,20 @@
+<?php
+
+	include("../dbCon.php'");
+	include("../app/lib/funcoes.php'");
+
+	$usuario	= $_GET["usuario"];
+	//$senha		= tratarStr($_GET["senha"]);
+	$senha		= $_GET["senha"];
+
+	$sql		= "SELECT * FROM usuario" WHERE nome = '" . $usuario . "' AND senha = '" . $senha . "';
+	$ret 		= mysqli_query($conexao, $sql);
+	
+	if(mysqli_num_rows($ret) > 0){
+		echo "Acesso ao sistema liberado!";
+	}else{
+		echo "Acesso ao sistema negado!";
+	}
+
+
+?>
