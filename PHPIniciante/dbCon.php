a64b7e6e4f0c4171a8be17be1510804d7f9b9301
ARCassiano <anderson.cassiano@hotmail.com>
2018-06-17 21:03:42 -0300

    Criando conexão utilizando PDO

--- PHPIniciante/dbCon.php
+++ PHPIniciante/dbCon.php
@@ -6,4 +6,10 @@ $db_user	= "u432556926_exe01";
 $db_pass	= "u432556926";
 $db_name 	= "u432556926_exe01";
 
-$conexao = @mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+$conexao	= @mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+try{
+	$pdo	= new PDO('mysql:host=' . $db_host . ';port=3306;dbname=' . $db_name, $db_user, $db_pass);	
+}catch(PDOException $e){
+	echo($e->getMessage());
+}
---
+++ PHPIniciante/pdo/basico.php
@@ -0,0 +1,7 @@
+<?php
+
+require("../dbCon.php");
+
+
+
+?>
