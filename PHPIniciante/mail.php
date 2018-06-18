1b95d194e0d3b0055f1739d1144bedfbe3369d1f
ARCassiano <anderson.cassiano@hotmail.com>
2018-06-18 20:54:39 -0300

    Enviando e-mail utilizando a função  mail()

--- PHPIniciante/mail.php
+++ PHPIniciante/mail.php
@@ -1,7 +1,9 @@
 <?php
 
-	$headers	= "From: PHP Iniciante <anderson.cassiano@hotmail.com>";
-
+	$headers	= "";
+	$headers	.= "MIME-Version: 1.0 \r\n";
+	$headers	.= "Content-type: text/html; charset=\"UTF-8\" \r\n";
+	$headers	.= "From: PHP Iniciante <anderson.cassiano@hotmail.com>";
 	if(mail("anderson.cassiano@hotmail.com", "olá", "teste", $headers)){
 		echo "E-mail enviado com sucesso!";
 	}else{
