73617cab16c0e746d859556949becd23554ca1be
ARCassiano <anderson.cassiano@hotmail.com>
2018-06-18 22:38:55 -0300

    Envio de e-mail autenticado

---
+++ PHPIniciante/mail-autenticado.php
@@ -0,0 +1,27 @@
+<?php
+
+	$headers	= "";
+	$headers	.= "MIME-Version: 1.0 \r\n";
+	$headers	.= "Content-type: text/html; charset=\"UTF-8\" \r\n";
+	$headers	.= "From: PHP Iniciante <anderson.cassiano@hotmail.com>";
+
+	$mensagem	= "<strong>Olá Anderson Cassiano</strong>";
+
+	include("app/lib/smpt/SMTPConfig.php");
+	include("app/lib/smpt/SMTPClass.php");
+
+	/*
+	* $smtpSever;
+	* $smtPort;
+	* $smtpUser;
+	* $smtpPas;
+	*/
+	$SMTPMail	= new SMTPClient($smtpSever, $smtPort, $smtpUser, $smtpPass, $smtpUser, $smtpUser, "E-mail enviado através do site", $mensagem, $headers);
+
+
+	if($SMTPMail->sendMail()){
+		echo("O E-mail foi enviado com sucesso!");
+	}else{
+		echo("O envio falhou!");
+	}
+?>
