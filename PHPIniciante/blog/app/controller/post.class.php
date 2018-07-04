2b05ddd2bbd8833541fcc2d4c83915ee9f03ef83
ARCassiano <anderson.cassiano@hotmail.com>
2018-07-04 20:47:30 -0300

    Projeto Final - Gerenciando posts

--- PHPIniciante/blog/app/controller/post.class.php
+++ PHPIniciante/blog/app/controller/post.class.php
@@ -12,7 +12,7 @@
 			
 			$criadoem = dtToBr($obj->postcriadoem);
 			$postdata = dtToBr($obj->postdata);
-			print_r($criadoem);
+			
 			$param = array("titulo"=>$app->site_titulo, 
 						   "pagina" => "formpost",
 						   "dados" => array(
